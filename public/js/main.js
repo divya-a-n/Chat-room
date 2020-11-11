26071f80e8537640f783a40616bc32cc234dbeb3
A <ADI1@novartis.net>
2020-11-11 10:34:22 +0530

    Initial commit

---
+++ public/js/main.js
@@ -0,0 +1,72 @@
+const chatForm = document.getElementById('chat-form')
+const chatMessages = document.querySelector('.chat-messages')
+const roomName = document.getElementById('room-name')
+const userList = document.getElementById('users')
+// Get username and room from URL
+const { username, room } = Qs.parse(location.search, {
+    ignoreQueryPrefix: true
+})
+console.log(username, room)
+
+
+const socket = io()
+
+// Join chatroom
+socket.emit('joinRoom', { username, room })
+
+// Get room and users
+socket.on('roomUsers', ({room, users}) => {
+    outputRoomName(room)
+    outputUsers(users)
+})
+
+// Message from server
+socket.on('message', message => {
+    console.log(message)
+    outputMessage(message)
+
+    //scroll down
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+})
+
+// Message submit
+chatForm.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    // Get message text
+    const msg = e.target.elements.msg.value;
+    
+    // Emit message to server
+    socket.emit('chatMessage',msg)
+})
+
+// Output Message to DOM
+function outputMessage(message) {
+    const div = document.createElement('div')
+    div.classList.add('message')
+    const p = document.createElement('p');
+    p.classList.add('meta');
+    p.innerText = `${message.username} `;
+    p.innerHTML += `<span>${message.time}</span>`;
+    div.appendChild(p);
+    const para = document.createElement('p');
+    para.classList.add('text');
+    para.innerText = `${message.text}`;
+    div.appendChild(para);
+    
+    document.querySelector('.chat-messages').appendChild(div); //ADDS(appends) THE MESSAGES IN THE CHAT BOX with classname chat-message
+}
+
+// Add room name to DOM
+function outputRoomName(room) {
+   roomName.innerText = room
+}
+
+// Add users to DOM
+function outputUsers(users) {
+    userList.innerHTML = '';
+    users.forEach(user=>{
+      const li = document.createElement('li');
+      li.innerText = user.username;
+      userList.appendChild(li);
+    });
+   }
---
+++ utils/users.js
@@ -0,0 +1,35 @@
+const users = []
+
+//join user to chat
+function userJoin(id, username, room) {
+    const user = { id, username, room }
+
+    users.push(user) //push user in the users array and return
+    return user
+}
+
+//Get current user
+function getCurrentUser(id) {
+    return users.find(user => user.id === id)
+}
+ //User leaves chat
+ function userLeave(id) {
+     const index = users.findIndex(user => user.id === id)
+     console.log(index)
+     if(index !== -1){
+         return users.splice(index, 1)[0]
+     }
+ }
+
+// Get room users
+function getRoomUsers(room) {
+    return users.filter(user => user.room === room)
+}
+
+
+module.exports = {
+    userJoin,
+    getCurrentUser,
+    getRoomUsers,
+    userLeave
+}
